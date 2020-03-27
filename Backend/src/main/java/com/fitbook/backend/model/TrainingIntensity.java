a17e529aed934450fae126157310f31f67de7fdc
Dominikoo <49551590+Dominikoo@users.noreply.github.com>
2020-03-27 18:23:21 +0100

    add training intensity table definition

---
+++ Backend/src/main/java/com/fitbook/backend/model/TrainingIntensity.java
@@ -0,0 +1,47 @@
+package com.fitbook.backend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Training_Plans")
+public class TrainingIntensity {
+
+    @Id
+    @GeneratedValue(generator = "training_intensity_generator")
+    @SequenceGenerator(
+            name = "training_intensity_generator",
+            sequenceName = "training_intensity_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @Column(columnDefinition = "text", unique = true)
+    private String code;
+
+    @Column(columnDefinition = "text", unique = false)
+    private String displayName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
