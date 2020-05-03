91208898d280f19e743c37bf15dbd1a3384b1488
lilraisin <olehsander.karonski@gmail.com>
2020-05-03 14:56:37 +0200

    Add userTrainingManagement tests

--- Frontend/Fitbook-frontend/src/app/pages/user-training-management/user-training-management.component.spec.ts
+++ Frontend/Fitbook-frontend/src/app/pages/user-training-management/user-training-management.component.spec.ts
@@ -6,8 +6,18 @@ import {
   FormGroup,
   FormArray,
   FormControl,
-  ValidatorFn
+  ValidatorFn,
+  FormsModule,
+  ReactiveFormsModule
 } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader';
+import { PositioningService } from 'ngx-bootstrap/positioning';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+
 
 describe('UserTrainingManagementComponent', () => {
   let component: UserTrainingManagementComponent;
@@ -15,7 +25,18 @@ describe('UserTrainingManagementComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserTrainingManagementComponent ]
+      imports:[
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ UserTrainingManagementComponent ],
+      providers: [
+        BsModalService,
+        ComponentLoaderFactory,
+        PositioningService
+      ]
     })
     .compileComponents();
   }));
@@ -24,9 +45,45 @@ describe('UserTrainingManagementComponent', () => {
     fixture = TestBed.createComponent(UserTrainingManagementComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    localStorage.setItem('userLogin', 'login1');
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call submit method', () => {
+    fixture.detectChanges();
+    spyOn(component, 'submit');
+
+    let el = fixture.debugElement.query(By.css('#submit-button')).nativeElement;
+    el.click();
+    expect(component.submit).toHaveBeenCalledTimes(1);
+  });
+  
+  it('should call addTrainingPlanPopupOpen method', () => {
+    fixture.detectChanges();
+    spyOn(component, 'addTrainingPlanPopupOpen');
+
+    let el = fixture.debugElement.query(By.css('#create-training-button')).nativeElement;
+    el.click();
+    expect(component.addTrainingPlanPopupOpen).toHaveBeenCalledTimes(1);
+  });
+  
+  it('should call sendTraininigInfo method', () => {
+    fixture.detectChanges();
+    spyOn(component, 'sendTraininigInfo');
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      let de = fixture.debugElement.queryAll(By.css('.train-button'));
+      console.log(de);
+      let el = fixture.debugElement.query(By.css('.retrain-button')).nativeElement;
+      el.click();
+      console.log(el)
+      expect(component.sendTraininigInfo).toHaveBeenCalledTimes(9);
+    })
+  });
+
 });
