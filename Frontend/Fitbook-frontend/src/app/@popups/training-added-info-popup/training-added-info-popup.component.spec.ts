c661a5e900b73f89203ded50516a0051250fdf5a
lilraisin <olehsander.karonski@gmail.com>
2020-05-09 21:46:54 +0200

    Test for trainingAddedInfoPopup

--- Frontend/Fitbook-frontend/src/app/@popups/training-added-info-popup/training-added-info-popup.component.spec.ts
+++ Frontend/Fitbook-frontend/src/app/@popups/training-added-info-popup/training-added-info-popup.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrainingAddedInfoPopupComponent } from './training-added-info-popup.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { By } from '@angular/platform-browser';
 
 describe('TrainingAddedInfoPopupComponent', () => {
   let component: TrainingAddedInfoPopupComponent;
@@ -8,7 +12,14 @@ describe('TrainingAddedInfoPopupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TrainingAddedInfoPopupComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      declarations: [ TrainingAddedInfoPopupComponent ],
+      providers: [
+        BsModalRef
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +33,40 @@ describe('TrainingAddedInfoPopupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set onClose to true', (done) => {
+    component.onClose.subscribe(response => {
+      expect(response).toEqual(true);
+      done();
+    })
+    component.onConfirm();
+  });
+
+  it('should set onClose to false', (done) => {
+    component.onClose.subscribe(response => {
+      expect(response).toEqual(false);
+      done();
+    })
+    component.onCancel();
+  });
+
+  it('should call the onConfirm method', async(() => {
+    spyOn(component, 'onConfirm');
+
+    let el = fixture.debugElement.query(By.css('.confirm-button')).nativeElement;
+    el.click();
+
+    expect(component.onConfirm).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should call the onCancel method', async(() => {
+    spyOn(component, 'onCancel');
+
+    let el = fixture.debugElement.queryAll(By.css('.cancel-button'));
+    el.forEach(element => {
+      element.nativeElement.click()
+    });
+
+    expect(component.onCancel).toHaveBeenCalledTimes(2);
+  }));
 });
