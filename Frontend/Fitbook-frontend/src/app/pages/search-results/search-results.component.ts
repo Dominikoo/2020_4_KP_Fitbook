0bdc19d3d6f10ea8032b2ef9c77200cd4901ee43
Dominikoo <49551590+Dominikoo@users.noreply.github.com>
2020-05-17 13:23:50 +0200

    add searched users list

--- Frontend/Fitbook-frontend/src/app/pages/search-results/search-results.component.ts
+++ Frontend/Fitbook-frontend/src/app/pages/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from './../../services/search.service';
+import { UserConnectionService } from './../../services/user.connection.service';
 
 @Component({
   selector: 'app-search-results',
@@ -9,14 +10,37 @@ import { SearchService } from './../../services/search.service';
 export class SearchResultsComponent implements OnInit {
 
   searchedUsers: any;
+  count = 0;
 
-  constructor(private searchService: SearchService) { }
+  constructor(private searchService: SearchService,
+              private userConnectionService: UserConnectionService) { }
 
   ngOnInit(): void {
-    this.searchService.searchUsers(localStorage.getItem("phrase")).subscribe(response =>{ 
+    this.searchService.searchUserConnections(localStorage.getItem("phrase"), localStorage.getItem("userLogin")).subscribe(response =>{ 
       this.searchedUsers = response
+      this.count = this.searchedUsers.length
       console.log(this.searchedUsers)
     });
+  }
+
+  addFriend(item): void{
+    item.status = 2
+    this.userConnectionService.put(item).subscribe(response => item = response)
+  }
+
+  removeFriend(item): void{
+    item.status = 0
+    this.userConnectionService.put(item).subscribe(response => item = response)
+  }
+
+  cancelInvitation(item): void{
+    item.status = 0
+    this.userConnectionService.put(item).subscribe(response => item = response)
+  }
+
+  acceptInvitation(item): void{
+    item.status = 1
+    this.userConnectionService.put(item).subscribe(response => item = response)
   }
 
 }
