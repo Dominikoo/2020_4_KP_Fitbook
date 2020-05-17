0aa9c424e0c17478442319562de4f4b2c53ea521
Dominikoo <49551590+Dominikoo@users.noreply.github.com>
2020-05-17 13:22:45 +0200

    update SearchController

--- Backend/src/main/java/com/fitbook/backend/controller/SearchController.java
+++ Backend/src/main/java/com/fitbook/backend/controller/SearchController.java
@@ -1,6 +1,8 @@
 package com.fitbook.backend.controller;
 
 import com.fitbook.backend.model.User;
+import com.fitbook.backend.model.UserConnection;
+import com.fitbook.backend.repository.UserConnectionRepository;
 import com.fitbook.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -16,9 +20,18 @@ public class SearchController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserConnectionRepository userConnectionRepository;
 
-    @GetMapping("/auth/search/users/{phrase}")
-    public List<User> searchUsers(@PathVariable String phrase){
-        return userRepository.searchUsersByPhrase(phrase);
+    @GetMapping("/auth/search/userConnections/{phrase}/{userLogin}")
+    public List<UserConnection> searchUserConnections(@PathVariable String phrase, @PathVariable String userLogin){
+        User user = userRepository.getUserByLogin(userLogin);
+        List<User> foundUsers = userRepository.searchUsersByPhrase(phrase);
+        List<UserConnection> userConnections = new ArrayList<UserConnection>();
+        for(User foundUser : foundUsers){
+            UserConnection userConnection = userConnectionRepository.getConnectionByLogins(user.getLogin(), foundUser.getLogin());
+            userConnections.add(Objects.requireNonNullElseGet(userConnection, () -> new UserConnection(user, foundUser, 0)));
+        }
+        return userConnections;
     }
 }
