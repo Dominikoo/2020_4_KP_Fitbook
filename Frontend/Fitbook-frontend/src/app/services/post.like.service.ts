50b35bfe8b6eb5a239e3f97037553d76ff8940fe
lilraisin <olehsander.karonski@gmail.com>
2020-05-18 11:54:05 +0200

    Not finished text post

---
+++ Frontend/Fitbook-frontend/src/app/services/post.like.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostLikeService {
+  //private url = 'https://backend-fitbook.herokuapp.com/auth/postLike';
+  private url = 'http://localhost:8080/auth/postLike';
+  constructor(private httpClient: HttpClient) { }
+
+//   put(modifiedConnection) {
+//     return this.httpClient.put(this.url, modifiedConnection)
+//   }
+
+    postPostLike(newLike) {
+        return this.httpClient.post<any>(this.url, newLike);
+    }
+
+    getLikesByPostId(postId) {
+        return this.httpClient.get<any>(this.url+`/${postId}`);
+    }
+}
