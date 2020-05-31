2cb09be73ce9655ca7c4f14a387d873839c8ca7a
Dominikoo <49551590+Dominikoo@users.noreply.github.com>
2020-05-31 20:49:59 +0200

    change url to heroku

--- Frontend/Fitbook-frontend/src/app/services/url.ts
+++ Frontend/Fitbook-frontend/src/app/services/url.ts
@@ -1,6 +1,6 @@
 
 
 export class UrlContainer {
-    //public url = 'https://backend-fitbook.herokuapp.com';
-    public url = 'http://localhost:8080'; 
+    public url = 'https://backend-fitbook.herokuapp.com';
+    //public url = 'http://localhost:8080'; 
 }
