fdc7ce9adc55038bb32528a22d0778d215e0e28b
lilraisin <olehsander.karonski@gmail.com>
2020-06-07 18:47:02 +0200

    Another hotfix for trainingDetailsComponent

--- Frontend/Fitbook-frontend/src/app/pages/training-details/training-details.component.ts
+++ Frontend/Fitbook-frontend/src/app/pages/training-details/training-details.component.ts
@@ -20,7 +20,7 @@ export class TrainingDetailsComponent implements OnInit {
   onlyEditMode = false;
 
   training;
-  trainingSessions;
+  trainingSessions: any[] = [];
   progress: any[] = [];
   bsModalRef: BsModalRef;
 
